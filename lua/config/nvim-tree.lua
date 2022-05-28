34b16ffb78129765fc954198452d5d54becc87a7
jinzaizhichi <jinzaizhichi9888@gmail.com>
2022-05-28 17:35:57 +0800

    fix nvim-tree.lua

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -1,9 +1,15 @@
 -- lua/config/nvim-tree.lua
 --vim.g.render.indent_markers = 1
  --vim.g.nvim_tree_indent_markers = 1
-vim.g.nvim_tree_add_trailing = 1
-vim.g.nvim_tree_special_files = { "README.md", "Makefile", "MAKEFILE", "package.json", "composer.json" }
-vim.g.nvim_tree_show_icons = { git = 1, folders = 1, files = 1, folder_arrows = 1 }
+--vim.g.nvim_tree_add_trailing = 1
+-- vim.g.nvim_tree_add_trailing = 1
+-- vim.g.nvim_tree_special_files = { "README.md", "Makefile", "MAKEFILE", "package.json", "composer.json" }
+-- vim.g.nvim_tree_show_icons = {
+--     git = 1,
+--     folders = 1,
+--     files = 1,
+--     folder_arrows = 1
+-- }
 
 require("nvim-tree").setup({
     diagnostics = {
@@ -23,6 +29,11 @@ require("nvim-tree").setup({
         },
    },
     renderer = {
+        add_trailing = false,
+        group_empty = false,
+        highlight_git = false,
+        highlight_opened_files = "none",
+        root_folder_modifier = ":~",
         indent_markers = {
             enable = false,
             icons = {
